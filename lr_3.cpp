cd517eec29af3b600c2fb2784fd104888f305204
Anny <nowhyix@mail.ru>
2022-12-06 02:14:44 +1000

    Initial commit

---
+++ lr_3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+/*
+Дан текстовый файл. С консоли считывается строка.
+Найти номера всех строк, в которых встречается данная подстрока.
+*/
+
+int main()
+{
+    const int max = 50;
+    char str[max];
+    int i = 0;
+    int pos;
+    string instr;
+    string tempstr;
+    cout << "Enter your love letter:";
+    cin >> instr;
+    ifstream file;
+    file.open("in.txt");
+
+    while (!file.eof())
+    {
+        file.getline(str, max); 
+        tempstr = str;
+        pos = tempstr.find(instr);
+        i++;
+        if (pos != -1)
+        {
+            cout << i << endl;
+        }
+    }
+    file.close();
+    return 0;
+}
+
+
